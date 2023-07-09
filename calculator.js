b6d1d6258ed1f88404028e154be7f0e62272989f
alexrnguyen <alexander.nguyen@shaw.ca>
2023-07-09 17:49:45 -0600

    Remove key listeners

--- calculator.js
+++ calculator.js
@@ -1,18 +1,43 @@
 const validKeys = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', Math.PI];
 const validOperators = ['+', '-', '×', '÷', '^'];
 
+/**
+ * Add 2 numbers a and b
+ * @param {number} a First number
+ * @param {number} b Second number 
+ * @returns Sum of a and b
+ */
 function add(a, b) {
     return a + b;
 }
 
+
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns 
+ */
 function subtract(a, b) {
     return a - b;
 }
 
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns 
+ */
 function multiply(a, b) {
     return a * b;
 }
 
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns 
+ */
 function divide(a, b) {
     if (b === 0) {
         return 'DIVIDE BY 0 ERROR'
@@ -20,14 +45,30 @@ function divide(a, b) {
     return a / b;
 }
 
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns 
+ */
 function power(a, b) {
     return a ** b;
 }
 
+/**
+ * 
+ * @param {number} a 
+ * @returns 
+ */
 function percentage(a) {
     return a/100;
 }
 
+/**
+ * Update the operands, operator, and expression based on the given input and the current state of the
+ * operands and operator.
+ * @param {string} input User input
+ */
 function processInput(input) {
 
     // Convert input of π to float representation in JavaScript
@@ -53,17 +94,28 @@ function processInput(input) {
         operator = convertOperator(input);
     }
 
-    const display = document.querySelector('.expression');
-
     // Convert input of π back to string representation to display on calculator screen
     if (input === Math.PI) {
         input = 'π';
     }
     expression += input;
-    display.textContent = expression;
-    console.table([operand1, operator, operand2, expression]);
+    updateDisplay(expression);
 }
 
+/**
+ * Updates the expression displayed on the calculator screen
+ * @param {*} expression The expression to be evaluated by the calculator
+ */
+function updateDisplay(expression) {
+    const display = document.querySelector('.expression');
+    display.textContent = expression;
+}
+
+/**
+ * Convert input displayed on screen to input that JavaScript can easily interpret
+ * @param {string} input User input
+ * @returns A string that can be easily interpreted
+ */
 function convertOperator(input) {
     if (input === '×') {
         return '*';
@@ -76,7 +128,9 @@ function convertOperator(input) {
     }
 }
 
-
+/**
+ * Performs an operation on the operands given an operator
+ */
 function operate() {
     let result = 0;
     operand1 = Number(operand1);
@@ -111,10 +165,22 @@ function operate() {
     operand2 = '';
     operator = '';
 
+    displayAnswer(result);
+}
+
+/**
+ * Displays the result on the calculator screen
+ * @param {*} result The result of an evaluated expression
+ */
+function displayAnswer(result) {
     const answer = document.querySelector('.answer');
     answer.textContent = result;
 }
 
+/**
+ * Resets the calculator state by resetting both operand, the operator, and the expression displayed on 
+ * the calculator screen
+ */
 function clear() {
     const display = document.querySelector('.expression');
     const answer = document.querySelector('.answer');
@@ -138,6 +204,7 @@ const clearButton = document.querySelector('#clear');
 const percentButton = document.querySelector('#percentage');
 const decimalButton = document.querySelector('#decimal');
 
+// Button event listeners
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         processInput(button.textContent);
